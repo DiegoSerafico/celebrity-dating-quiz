8bf16fd667d445ca7ea68b1d311c5cf57cd28222
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-12 14:40:00 -0700

    add functionality to form and outputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    $("#tryAgain").hide();
+    $("#books").hide();
+    $("#channing").hide();
+    $("#margot").hide();
     const gender = $("#genderInput").val();
-    
+    const age = $("#ageInput").val();
+    const date = $("#dateInput").val();
+
+    if (isNaN(age) || age < 1) {
+      $("#tryAgain").show();
+    } else if (age < 18) {
+      $("#books").show();
+    } else if (gender === "Both" && date === "Fancy dinner date") {
+      $("#margot").show();
+    } else if (gender === "Both" && date === "Night out at the club/bar") {
+      $("#channing").show();
+    } else if (gender === "Female") {
+      $("#margot").show();
+    } else {
+      $("#channing").show();
+    }
   });
 });
